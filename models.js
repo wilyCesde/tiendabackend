25e42a77fe0d0d0980204c0cbfec3f04a68f4513
wilyCesde <wruizza@cesde.net>
2023-05-29 14:56:52 -0500

    se crean las nuevas tablas

--- models.js
+++ models.js
@@ -8,13 +8,38 @@ const Articulo = mongoose.model('Articulo',
   new mongoose.Schema({ nombre: String, precio: Number })
 );
 
-module.exports = {
-  Cliente: Cliente,
-  Articulo: Articulo
-}
+const User = mongoose.model('User',
+  new mongoose.Schema({ username: String, password: String, name: String, role: String })
+);
 
-// Otra forma más corta:
-// module.exports = {
-//     Cliente,
-//     Articulo
-// }
+const Car = mongoose.model('Car',
+  new mongoose.Schema({ plateNumber: String, brand: String, state: String, dailyValue: Number })
+);
+
+const Rent = mongoose.model('Rent',
+  new mongoose.Schema({
+    rentNumber: String,
+    username: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    plateNumber: { type: mongoose.Schema.Types.ObjectId, ref: 'Car' },
+    initialDate: Date,
+    finalDate: Date,
+    status: String,
+  })
+);
+
+const ReturnCar = mongoose.model('ReturnCar',
+  new mongoose.Schema({
+    returnNumber: String,
+    rentNumber: { type: mongoose.Schema.Types.ObjectId, ref: 'Rent' },
+    returnDate: Date,
+  })
+);
+
+module.exports = {
+  Cliente,
+  Articulo,
+  User,
+  Car,
+  Rent,
+  ReturnCar
+};
